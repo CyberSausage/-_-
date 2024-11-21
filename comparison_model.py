79ffebb6b7946f56be8b8dfb775d527f62c1ec7e
CyberSausage <midoni.stas@mail.ru>
2024-11-22 01:21:18 +0200

    bug fix with GIF format

--- comparison_model.py
+++ comparison_model.py
@@ -1,4 +1,6 @@
 
+from PIL import Image
+
 from skimage.metrics import structural_similarity as ssim
 import cv2
 
@@ -16,6 +18,14 @@ class Similarity_Model:
         img1 = cv2.imread(path_img1)
         img2 = cv2.imread(path_img2)
 
+        if img1 is None:
+            self.convert_to_png(path_img1, path_img1)
+            img1 = cv2.imread(path_img1)
+
+        if img2 is None:
+            self.convert_to_png(path_img2, path_img2)
+            img2 = cv2.imread(path_img2)
+
         img2_resized = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
 
         gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -24,3 +34,8 @@ class Similarity_Model:
         similarity, _ = ssim(gray_img1, gray_img2, full=True)
 
         return similarity if similarity > 0 else 0
+
+    def convert_to_png(self, input_path, output_path):
+        print(input_path)
+        with Image.open(input_path) as img:
+            img.save(output_path, format="PNG")
